9e4c0c1d2c5bf82f3324697df8e02fb678ec6957
Andrew <andrew.sideby@gmail.com>
2024-01-07 15:20:03 +0800

    ton master v1.1

--- api/message.ts
+++ api/message.ts
@@ -1,18 +1,12 @@
-import {Bot} from "grammy";
-import {MyContext, MyConversation} from "./global.types";
-
+import { Bot } from "grammy";
+import { MyContext, MyConversation } from "./global.types";
 
 export function on_message(bot: Bot<MyContext>) {
-    bot.on(
-        "message",
-        async (ctx) => {
-
-            if (ctx.message.text === 'name') {
-                // await ctx.reply(" 导航？？ " + ctx.msg.text + ctx.senderChat?.id + "from: " + ctx.from, {  reply_markup: inlineKeyboard,})
-            }
-
-            // if (ctx.config.isDeveloper) await ctx.reply("Hi Admin! from msg")
-            ctx.reply("get a message = " + ctx.message.text).then(() => {})
-        },
-    );
+  bot.on("message", async (ctx) => {
+    // if (ctx.message.text === "name") {
+    //   // await ctx.reply(" 导航？？ " + ctx.msg.text + ctx.senderChat?.id + "from: " + ctx.from, {  reply_markup: inlineKeyboard,})
+    // }
+    // if (ctx.config.isDeveloper) await ctx.reply("Hi Admin! from msg")
+    // ctx.reply("get a message = " + ctx.message.text).then(() => {})
+  });
 }
--- vercel-webhook.md
+++ vercel-webhook.md
@@ -1,1 +1,1 @@
-https://api.telegram.org/bot6848653110:AAFuVZe_OBc0zAgoBfpa8mT7SrW0yrC1okE/setWebhook?url=https://ton-master-bot.vercel.app/api/bot
+https://api.telegram.org/bot6974828212:AAFZzNoM6Wsv8jI75JsZ8ukbzVuy-CtFvB8/setWebhook?url=https://ton-master-bot.vercel.app/api/bot
