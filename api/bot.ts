5c04105e9c1739bbb2767e212509f4afa3b94e6f
Andrew <andrew.sideby@gmail.com>
2024-01-07 17:53:50 +0800

    // Vercel analysis inject

--- api/bot.ts
+++ api/bot.ts
@@ -1,25 +1,26 @@
-import {Bot, session, webhookCallback} from "grammy";
-import {SocksProxyAgent} from "socks-proxy-agent";
-import {bind_command} from "./command";
-import {on_message} from "./message";
-import {use_middleware} from "./ctx.config";
-import {conversations, createConversation} from "@grammyjs/conversations";
-import {use_conv} from "./conv";
-import {MyContext} from "./global.types";
+import { Bot, session, webhookCallback } from "grammy";
+import { SocksProxyAgent } from "socks-proxy-agent";
+import { bind_command } from "./command";
+import { on_message } from "./message";
+import { use_middleware } from "./ctx.config";
+import { conversations, createConversation } from "@grammyjs/conversations";
+import { use_conv } from "./conv";
+import { MyContext } from "./global.types";
+import { inject } from "@vercel/analytics";
 
 // ===========================================================================
 //                        Bot Init Section Start
 // ===========================================================================
 let config = {};
-if (process.env.NODE_ENV === 'dev') {
-    config = {
-        client: {
-            baseFetchConfig: {
-                agent: new SocksProxyAgent("socks://127.0.0.1:7890"),
-                compress: true,
-            },
-        },
-    }
+if (process.env.NODE_ENV === "dev") {
+  config = {
+    client: {
+      baseFetchConfig: {
+        agent: new SocksProxyAgent("socks://127.0.0.1:7890"),
+        compress: true,
+      },
+    },
+  };
 }
 const token = process.env.BOT_TOKEN;
 if (!token) throw new Error("BOT_TOKEN is unset");
@@ -28,34 +29,40 @@ export const bot = new Bot<MyContext>(token, config);
 //                        Bot Init Section End
 // ===========================================================================
 
-use_middleware(bot)
+use_middleware(bot);
 bind_command(bot);
 // use_conv(bot);
 on_message(bot);
 
-
 // ===========================================================================
 //                        Startup Section Start
 // ===========================================================================
-let CallbackExport = {};
-if (process.env.NODE_ENV === 'dev') {
-    // Stopping the bot when the Node.js process
-    // is about to be terminated
-    process.once("SIGINT", () => bot.stop());
-    process.once("SIGTERM", () => bot.stop());
-    bot.start().then((e) => {
-        console.info(e)
-    }).catch((e) => {
-        console.error(e)
-    });
-} else {
-    CallbackExport = webhookCallback(bot, "http");
+// Vercel analysis inject
+try {
+  inject();
+} catch (e) {
+  console.error(e);
 }
 
-export default CallbackExport
+let CallbackExport = {};
+if (process.env.NODE_ENV === "dev") {
+  // Stopping the bot when the Node.js process
+  // is about to be terminated
+  process.once("SIGINT", () => bot.stop());
+  process.once("SIGTERM", () => bot.stop());
+  bot
+    .start()
+    .then((e) => {
+      console.info(e);
+    })
+    .catch((e) => {
+      console.error(e);
+    });
+} else {
+  CallbackExport = webhookCallback(bot, "http");
+}
+
+export default CallbackExport;
 // ===========================================================================
 //                        Startup Section End
 // ===========================================================================
-
-
-
